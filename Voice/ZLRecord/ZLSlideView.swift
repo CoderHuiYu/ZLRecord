3ee1d3b91f56923d8c00624229fcedf047893576
yuhui <171364980@qq.com>
2018-12-05 15:28:02 +0800

    修改滑动来删除的富文本

--- Voice/ZLRecord/ZLSlideView.swift
+++ Voice/ZLRecord/ZLSlideView.swift
@@ -16,69 +16,56 @@ class ZLSlideView: UIView {
     
     lazy var showLabel : UILabel = {
         let label = UILabel.init(frame: self.bounds)
-        label.text = "滑动来取消"
         label.font = UIFont.systemFont(ofSize: 16)
         label.textColor = UIColor.init(red: 20/255.0, green: 20/255.0, blue: 20/255.0, alpha: 1)
         label.textAlignment = NSTextAlignment.center
-//        label.backgroundColor = UIColor.init(displayP3Red: 200/255.0, green: 200/255.0, blue: 200/255.0, alpha: 1)
-       
         return label
-    }()
-    
-    lazy var arrowImageView : UIImageView = {
-        let imgView = UIImageView.init(image: UIImage.init(named: "SlideArrow"))
-        var frame = imgView.frame
-        frame.size = CGSize.init(width: 8, height: 17)
-        frame.origin.x = self.frame.size.width/2.0 + 45
-        frame.origin.y = (self.frame.height - frame.height)/2
-        imgView.frame = frame
-        return imgView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         isUserInteractionEnabled = true
-        
         addSubview(showLabel)
-        addSubview(arrowImageView)
+        addAttribute()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func addAttribute() {
+      
+        let attri : NSMutableAttributedString = NSMutableAttributedString.init(string: "滑动来取消")
+        let  attach : NSTextAttachment = NSTextAttachment.init()
+        attach.image = UIImage.init(named: "SlideArrow")
+        attach.bounds = CGRect.init(x: 5, y:(17-self.frame.size.height/2)/2, width: 8, height: 17)
+        let string = NSAttributedString.init(attachment: attach)
+        attri.append(string)
+        showLabel.attributedText = attri;
+    }
+    
     func resetFrame() {
         showLabel.frame = CGRect.init(origin: self.bounds.origin, size: self.bounds.size)
-        var frame  = CGRect.init()
-        frame.size = CGSize.init(width: 8, height: 17)
-        frame.origin.x = self.frame.size.width/2.0 + 45
-        frame.origin.y = (self.frame.height - frame.height)/2
-        arrowImageView.frame = frame
+        addAttribute()
     }
     
     func updateLocation(_ offSetX : CGFloat) {
-//        print("offSetX \(offSetX)")
         var labelFrame = showLabel.frame
         labelFrame.origin.x += offSetX
         showLabel.frame = labelFrame
-        
-        var imgViewFrame = arrowImageView.frame
-        imgViewFrame.origin.x += offSetX
-        arrowImageView.frame = imgViewFrame
     }
     
     func changeStatus() {
         showLabel.text = "取消"
         showLabel.isUserInteractionEnabled = true
         showLabel.textColor = commonBlueColor
-        arrowImageView.isHidden = true
-        
+
         let tap : UITapGestureRecognizer = UITapGestureRecognizer.init(target: self, action:#selector(canelRecord))
         showLabel.addGestureRecognizer(tap)
     }
     
     func resetShowLableText() {
-        showLabel.text = "滑动来取消"
+        addAttribute()
         showLabel.font = UIFont.systemFont(ofSize: 16)
         showLabel.textColor = UIColor.init(red: 20/255.0, green: 20/255.0, blue: 20/255.0, alpha: 1)
         showLabel.textAlignment = NSTextAlignment.center
